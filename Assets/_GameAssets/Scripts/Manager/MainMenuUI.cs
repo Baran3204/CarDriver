2242419ba6c0101952c75ae02ac138e3245bb112
Baran3204 <brngumus1345@gmail.com>
2025-03-09 17:32:55 +0300

    Timer Added

--- Assets/_GameAssets/Scripts/Manager/MainMenuUI.cs
+++ Assets/_GameAssets/Scripts/Manager/MainMenuUI.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections;
+using System.Linq;
 using DG.Tweening;
 using TMPro;
+using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,8 +17,8 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] private PlayerController _playerController;
 
     [Header("References Texts")]
-    [SerializeField] private TMP_Text[] _texts;
-
+    [SerializeField] private RectTransform[] _transforms;
+    private int _currentİndexTransform;
     private void Awake() 
     {
         _startButton.onClick.AddListener(StartButtonClicked);    
@@ -38,6 +41,24 @@ public class MainMenuUI : MonoBehaviour
     private void StartButtonClicked()
     {
         _mainMenuTransform.DOScale(0f, 0.5f).SetEase(Ease.InBack);
-        _playerController.ChangeGameOver(false);
+        StartCoroutine(nameof(StartTimer));
+        
+    }
+
+    private IEnumerator StartTimer()
+    {
+        for(int i = 0; i <= 3; i++)
+        {
+          RectTransform currentTransform = _transforms[_currentİndexTransform];
+
+          currentTransform.DOScale(1f, 0.5f).SetEase(Ease.OutBack);
+
+          yield return new WaitForSeconds(1f);
+
+          currentTransform.DOScale(0f, 0.5f).SetEase(Ease.InBack);
+     
+          _currentİndexTransform++;          
+        }
+        _playerController.ChangeGameOver(false);   
     }
 }
